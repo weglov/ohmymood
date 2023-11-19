f1618a144cea59b0665a9d5cbaaa0fddeb431a06
Viktor <ruscheglov@gmail.com>
2023-11-19 23:57:42 +0300

    remove pwa mode

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
-const withPWA = require('next-pwa')
 /** @type {import('next').NextConfig} */
 
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
-  pwa: {
-    dest: 'public',
-  },
-})
+}
 
 module.exports = nextConfig
